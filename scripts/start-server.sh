471a0d743a6cd6d3fba223f36e48503882dcf6fe
minseonjae <msj6785@naver.com>
2025-05-03 23:03:46 +0900

    github action tes12

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo "--------------- 서버 배포 시작 -----------------"
-cd /home/ubuntu/minseonjae-server
-sudo fuser -k -n tcp 8080 || true
-nohup java -jar project.jar > ./output.log 2>&1 &
-echo "--------------- 서버 배포 끝 -----------------"
+docker stop minseonjae-server || true
+docker rm minseonjae-server || true
+docker pull 940482451773.dkr.ecr.ap-northeast-2.amazonaws.com/minseonjae-server:latest
+docker run -d --name minseonjae-server -p 3000:3000 940482451773.dkr.ecr.ap-northeast-2.amazonaws.com/minseonjae-server:latest
+echo "--------------- 서버 배포 끝 ------------------"
